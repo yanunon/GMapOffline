abad2b91ca6ef48367b362759576a2184631a12e
Yang Junyong <yanunon@gmail.com>
2013-05-23 21:04:42 +0800

    重构

--- gmaps/mapDownloader.py
+++ gmaps/mapDownloader.py
@@ -10,58 +10,26 @@ import random
 import re
 import os
 import time
+import Image
+import Queue
 from threading import Thread, Lock
 
 import mapTool
 import urlFetcher
-import mapFile
 
+MAP_LAYERS = {
+          'map_cn': 'http://mt%d.google.cn/vt/lyrs=m@216173479&hl=zh-CN&gl=CN&src=app&x=%d&y=%d&z=%d&s=G',
+          'sat_cn': 'http://mt%d.google.cn/vt/lyrs=s@129&hl=zh-CN&gl=CN&src=app&x=%d&s=&y=%d&z=%d&s=Galileo',
+          'road_cn': 'http://mt%d.google.cn/vt/imgtp=png32&lyrs=h@216009069&hl=zh-CN&gl=CN&src=app&x=%d&s=&y=%d&z=%d&s=Galile',
+          }
 
-class DownloadMap(object):
-    '''
-    classdocs
-    '''
+MAP_LAYER_IMG_TYPE = {
+            'map_cn': 'png',
+            'sat_cn': 'jpg',
+            'road_cn': 'png',
+                       }
 
-    layer_url = {}
-
-    def __init__(self, http_proxy=None):
-        '''
-        Constructor
-        '''
-        self.fetcher = urlFetcher.UrlFetcher(http_proxy=http_proxy)
-        
-    #http://mt%d.google.com/vt/lyrs=m@163000000&hl=zh-CN&x=%d&y=%d&z=%i
-    def __parse_google_maps__(self, layer):
-        map_server_query = {"gmap":"m", "ghyb":"h", "gsat":"k", "gter":"p"}
-        basic_url = 'http://maps.google.com/maps?t=%s&hl=%s' % (map_server_query[layer], 'zh-CN')
-        
-        html = self.fetcher.do_fetch(basic_url)
-        
-        if not html:
-            return None
-        
-        hybrid = ''
-        if layer == 'ghyb':
-            hybrid = 'Hybrid'
-            
-        pattern = '<div id=inlineTiles'+ hybrid +' dir=ltr><img src="http://([a-z]{2,3})[0-9].google.com/(.+?)&'
-        match = re.search(pattern, html)
-        if match is not None:
-            #print tuple(match.groups())
-            return_url = 'http://%s%%d.google.com/%s' % tuple(match.groups())
-            return_url +='&hl=zh-CN&x=%d&y=%d&z=%i'
-            return return_url
-        else:
-            return None
-    
-    def download(self, layer, x, y, z):
-        if layer not in self.layer_url.keys():
-            self.layer_url[layer] = self.__parse_google_maps__(layer)
-        fetch_url = self.layer_url[layer] % (random.randrange(4), x, y, z)
-        #print fetch_url
-        return self.fetcher.do_fetch(fetch_url)
-
-class DownloadTask:
+class MapTile:
     
     def __init__(self, layer, x, y, z, overwrite=False):
         self.layer = layer
@@ -69,152 +37,143 @@ class DownloadTask:
         self.y = y
         self.z = z
         self.overwrite = overwrite
+    
+    def get_url(self):
+        url = None
+        if self.layer in MAP_LAYERS.keys():
+            url = MAP_LAYERS[self.layer] % (random.randrange(4), self.x, self.y, self.z)
+        return url
+    
+    def get_file_path(self):
+        world_tile = 1 << self.z
+        x = self.x % world_tile
+        folder_list = [self.layer, str(self.z), str(x/1024), str(x%1024), str(self.y/1024)]
+        file_name = "%d.%s" % (self.y%1024 , MAP_LAYER_IMG_TYPE[self.layer])
+        file_path = ''
+        for folder in folder_list:
+            file_path = os.path.join(file_path, folder)
+        file_path = os.path.join(file_path, file_name)
+        return file_path
+    
+def get_location_tiles(top_left, bottom_right, zoom, layer):
+    top_left_tile = mapTool.coord_to_tile((top_left[0], top_left[1], zoom))
+    bottom_right_tile = mapTool.coord_to_tile((bottom_right[0], bottom_right[1], zoom))
+    
+    xmin = top_left_tile[0][0]
+    ymin = top_left_tile[0][1]
+    
+    xmax = bottom_right_tile[0][0]
+    ymax = bottom_right_tile[0][1]
+    
+    world_tiles = 1 << zoom
+    if xmax - xmin >= world_tiles:
+        xmin, xmax = 0, world_tiles-1
+    if ymax - ymin >= world_tiles:
+        ymin, ymax = 0, world_tiles-1
 
-class TaskQueue:
+    tiles = []
+    for i in xrange((xmax - xmin+world_tiles)%world_tiles + 1):
+        x = (xmin + i)%world_tiles
+        for j in xrange((ymax - ymin+world_tiles)%world_tiles + 1):
+            y = (ymin + j)%world_tiles
+            tile = MapTile(layer, x, y, zoom)
+            tiles.append(tile)
     
-    def __init__(self):
-        self.stack = []
-        self.lock = Lock()
-        self.join_lock = Lock()
-        self.length = 0
-        self.stop_all = False
-        self.unfinished = 0
-        
-    def get(self):
-        task = -1
-        self.lock.acquire()
-        try:
-            if self.length > 0:
-                task = self.stack.pop()
-                self.length -= 1
-            elif self.stop_all:
-                task = None
-        finally:
-            self.lock.release()
-        
-#        self.join_lock.acquire()
-#        try:
-#            if self.stop_all:
-#                task = None
-#        finally:
-#            self.join_lock.release()
-            
-        return task
+    return tiles
     
-    def put(self, task):
-        self.lock.acquire()
-        try:
-            self.stack.append(task)
-            self.length += 1
-            self.unfinished += 1
-        finally:
-            self.lock.release()
+def merge_location_image(top_left, bottom_right, zoom, layer):
+    top_left_tile = mapTool.coord_to_tile((top_left[0], top_left[1], zoom))
+    bottom_right_tile = mapTool.coord_to_tile((bottom_right[0], bottom_right[1], zoom))
     
-    def getSize(self):
-        length = -1
-        self.lock.acquire()
-        try:
-            length = self.length
-        finally:
-            self.lock.release()
-        return length
+    xmin = top_left_tile[0][0]
+    ymin = top_left_tile[0][1]
     
-    def finish_one(self):
-        self.lock.acquire()
-        try:
-            self.unfinished -= 1
-        finally:
-            self.lock.release()
+    xmax = bottom_right_tile[0][0]
+    ymax = bottom_right_tile[0][1]
     
-    def join(self):
-        self.lock.acquire()
-        try:
-            while self.unfinished > 0:
-                self.lock.release()
-                time.sleep(0.1)
-                self.lock.acquire()
-        finally:
-            self.lock.release()
+    world_tiles = 1 << zoom
+    if xmax - xmin >= world_tiles:
+        xmin, xmax = 0, world_tiles-1
+    if ymax - ymin >= world_tiles:
+        ymin, ymax = 0, world_tiles-1
     
-    def stop(self):
-        self.join()
-        self.lock.acquire()
-        try:
-            self.stop_all = True
-            self.length = 0
-        finally:
-            self.lock.release()
-        
-    
+    width = (xmax - xmin+world_tiles)%world_tiles + 1
+    height = (ymax - ymin+world_tiles)%world_tiles + 1
+    map_image = Image.new("RGB", (width*256,height*256))
+    for i in xrange(width):
+        x = (xmin + i)%world_tiles
+        for j in xrange(height):
+            y = (ymin + j)%world_tiles
+            tile = MapTile(layer, x, y, zoom)
+            file_path = tile.get_file_path()
+            if os.path.exists(file_path):
+                paste_box = (i*256, j*256, (i+1)*256, (j+1)*256)
+                paste_im = Image.open(file_path)
+                map_image.paste(paste_im, paste_box)
+            if layer == 'sat_cn':
+                tile = MapTile('road_cn', x, y, zoom)
+                file_path = tile.get_file_path()
+                if os.path.exists(file_path):
+                    #paste_box = (i*256, j*256, (i+1)*256, (j+1)*256)
+                    paste_im = Image.open(file_path)
+                    map_image.paste(paste_im, paste_box, paste_im)
+    return map_image
+
 class DownloadThread(Thread):
     
-    def __init__(self, file_lock, task_queue, name=None):
+    def __init__(self, task_queue, lock, name=None, http_proxy=None):
         Thread.__init__(self, name=name)
-        self.downloadMap = DownloadMap()
-        self.lock = file_lock
+        self.fetcher = urlFetcher.UrlFetcher(http_proxy=http_proxy)
         self.task_queue = task_queue
+        self.folder_lock = lock
+        self.running = True
         
     def run(self):
-        while True:
-            task = self.task_queue.get()
-            if task is None:
-                return
-            elif task == -1:
-                time.sleep(0.1)
-                continue
-            
-            file_data = self.downloadMap.download(task.layer, task.x, task.y, task.z)
-            print 'Thread:%s download map layer %s x:%d y:%d z:%d' % (self.name, task.layer, task.x, task.y, task.z)
-            if file_data:
-                self.lock.acquire()
-                try:
-                    mapFile.save_map_file(file_data, task.x, task.y, task.z, task.layer, task.overwrite)
-                finally:
-                    self.lock.release()
-                self.task_queue.finish_one()
+        while self.running:
+            try:
+                task = self.task_queue.get(True, 1)
+                file_path = task.get_file_path()
+                if not os.path.exists(file_path):
+                    fetch_url = task.get_url()
+                    file_data = self.fetcher.do_fetch(fetch_url)
+                    folder_path = os.path.dirname(file_path)
+                    self.folder_lock.acquire()
+                    if not os.path.exists(folder_path):
+                        os.makedirs(folder_path)
+                    self.folder_lock.release()
+                    file = open(file_path, 'wb')
+                    file.write(file_data)
+                    file.close()
+                self.task_queue.task_done()
+            except Queue.Empty:
+                pass
+                
+                
+    def stop(self):
+        self.running = False
 
 class MapDownloader:
     
     def __init__(self, thread_num=5):
-        self.file_lock = Lock()
-        self.task_queue = TaskQueue()
+        self.task_queue = Queue.Queue()
         self.threads = []
         self.thread_num = thread_num
+        self.lock = Lock()
+        
         for i in range(self.thread_num):
-            thread = DownloadThread(self.file_lock, self.task_queue, '[%d]' % i)
+            thread = DownloadThread(self.task_queue, self.lock, '[%d]' % i)
             self.threads.append(thread)
             thread.start()
-            
-    def dl_region(self, xmin, xmax, ymin, ymax, zoom, layer):
-        world_tiles = 1 << zoom
-        if xmax - xmin >= world_tiles:
-            xmin, xmax = 0, world_tiles-1
-        if ymax - ymin >= world_tiles:
-            ymin, ymax = 0, world_tiles-1
-        
-        for i in xrange((xmax - xmin+world_tiles)%world_tiles + 1):
-            x = (xmin + i)%world_tiles
-            for j in xrange((ymax - ymin+world_tiles)%world_tiles + 1):
-                y = (ymin + j)%world_tiles
-                if not os.path.exists(mapFile.get_tile_path(layer, x, y, zoom)[1]):
-                    task = DownloadTask(layer, x, y, zoom)
-                    self.task_queue.put(task)
-        #for thread in self.threads:
-        #    thread.start()
                     
     def dl_location(self, top_left, bottom_right, zoom, layer):
-        top_left_tile = mapTool.coord_to_tile((top_left[0], top_left[1], zoom))
-        bottom_right_tile = mapTool.coord_to_tile((bottom_right[0], bottom_right[1], zoom))
-        
-        xmin = top_left_tile[0][0]
-        ymin = top_left_tile[0][1]
-        
-        xmax = bottom_right_tile[0][0]
-        ymax = bottom_right_tile[0][1]
-        self.dl_region(xmin, xmax, ymin, ymax, zoom, layer)
+        tiles = get_location_tiles(top_left, bottom_right, zoom, layer)
+        for tile in tiles:
+            self.task_queue.put(tile)
     
     def wait_thread_done(self):
         self.task_queue.join()
+        for thread in self.threads:
+            thread.stop()
     
     def stop_all(self):
         self.task_queue.stop()
@@ -224,15 +183,17 @@ class MapDownloader:
 if __name__ == '__main__':
     #d = downloadMap('gmap')
     d = MapDownloader(10)
-    top_left = (45, 80)
-    bottom_right = (30, 135)
-    layer = 'gmap'
-    zoom = 8
+    top_left = (39.922787,116.391925)
+    bottom_right = (39.914016,116.401452)
+    layer = 'sat_cn'
+    zoom = 18
     #time.sleep(10)
     d.dl_location(top_left, bottom_right, zoom, layer)
-    for thread in d.threads:
-        thread.join()
-    map = mapFile.merge_location_image(top_left, bottom_right, zoom, layer)
+    d.wait_thread_done()
+    
+    #for thread in d.threads:
+    #    thread.join()
+    map = merge_location_image(top_left, bottom_right, zoom, layer)
     map.show()
     map.save('test.png')
     
